10a7166e1dbb73161a690ced17c825199449167a
Sami Kallinen <sami@kpsystem.se>
2024-08-25 19:02:50 +0300

    format vpp data

--- src/jon30/data.clj
+++ src/jon30/data.clj
@@ -6,259 +6,90 @@
 ;; values manually measured from diagram.
 
 (def vpp-polar-01
-  [{:angle 0
-    :wind 6
-    :velocity 0}
-   {:angle 37.5
-    :wind 6
-    :velocity 3.345}
-   {:angle 45
-    :wind 6
-    :velocity 3.929}
-   {:angle 52.5
-    :wind 6
-    :velocity 4.345}
-   {:angle 60
-    :wind 6
-    :velocity 4.647}
-   {:velocity 4.817
-    :wind 6
-    :angle 67.5}
-   {:velocity 4.979
-    :wind 6
-    :angle 75}
-   {:velocity 4.914
-    :wind 6
-    :angle 82.5}
-   {:wind 6
-    :angle 90
-    :velocity 4.879}
-   {:wind 6
-    :angle 97.5
-    :velocity 4.768}
-   {:wind 6
-    :angle 105
-    :velocity 4.453}
-   {:wind 6
-    :angle 112.5
-    :velocity 4.143}
-   {:wind 6
-    :angle 120
-    :velocity 3.734}
-   {:wind 6
-    :angle 127.5
-    :velocity 3.375}
-   {:wind 6
-    :angle 135
-    :velocity 3.122}
-   {:wind 6
-    :angle 142.5
-    :velocity 3}
-   {:wind 6
-    :angle 150
-    :velocity 2.832}
-   {:wind 6
-    :angle 157.5
-    :velocity 2.812}
-   {:wind 6
-    :angle 165
-    :velocity 2.734}
-   {:wind 6
-    :angle 172.5
-    :velocity 2.734}
-   {:wind 6
-    :angle 180
-    :velocity 2.74}
+  [{:angle 0, :velocity 0, :wind 6}
+   {:angle 37.5, :velocity 3.345, :wind 6}
+   {:angle 45, :velocity 3.929, :wind 6}
+   {:angle 52.5, :velocity 4.345, :wind 6}
+   {:angle 60, :velocity 4.647, :wind 6}
+   {:angle 67.5, :velocity 4.817, :wind 6}
+   {:angle 75, :velocity 4.979, :wind 6}
+   {:angle 82.5, :velocity 4.914, :wind 6}
+   {:angle 90, :velocity 4.879, :wind 6}
+   {:angle 97.5, :velocity 4.768, :wind 6}
+   {:angle 105, :velocity 4.453, :wind 6}
+   {:angle 112.5, :velocity 4.143, :wind 6}
+   {:angle 120, :velocity 3.734, :wind 6}
+   {:angle 127.5, :velocity 3.375, :wind 6}
+   {:angle 135, :velocity 3.122, :wind 6}
+   {:angle 142.5, :velocity 3, :wind 6}
+   {:angle 150, :velocity 2.832, :wind 6}
+   {:angle 157.5, :velocity 2.812, :wind 6}
+   {:angle 165, :velocity 2.734, :wind 6}
+   {:angle 172.5, :velocity 2.734, :wind 6}
+   {:angle 180, :velocity 2.74, :wind 6}
+   {:angle 0, :velocity 0, :wind 10}
 
-   {:angle 0
-    :wind 10
-    :velocity 0}
-   {:angle 37.5
-    :wind 10
-    :velocity 4.489}
-   {:angle 45
-    :wind 10
-    :velocity 5.248}
-   {:angle 52.5
-    :wind 10
-    :velocity 5.607}
-   {:angle 60
-    :wind 10
-    :velocity 5.930}
-   {:velocity 5.989
-    :wind 10
-    :angle 67.5}
-   {:velocity 6.167
-    :wind 10
-    :angle 75}
-   {:velocity 6.155
-    :wind 10
-    :angle 82.5}
-   {:wind 10
-    :angle 90
-    :velocity 6.180}
-   {:wind 10
-    :angle 97.5
-    :velocity 6.070}
-   {:wind 10
-    :angle 105
-    :velocity 5.977}
-   {:wind 10
-    :angle 112.5
-    :velocity 5.767}
-   {:wind 10
-    :angle 120
-    :velocity 5.503}
-   {:wind 10
-    :angle 127.5
-    :velocity 5.087}
-   {:wind 10
-    :angle 135
-    :velocity 4.806}
-   {:wind 10
-    :angle 142.5
-    :velocity 4.659}
-   {:wind 10
-    :angle 150
-    :velocity 4.590}
-   {:wind 10
-    :angle 157.5
-    :velocity 4.456}
-   {:wind 10
-    :angle 165
-    :velocity 4.425}
-   {:wind 10
-    :angle 172.5
-    :velocity 4.395}
-   {:wind 10
-    :angle 180
-    :velocity 4.407}
+   {:angle 37.5, :velocity 4.489, :wind 10}
+   {:angle 45, :velocity 5.248, :wind 10}
+   {:angle 52.5, :velocity 5.607, :wind 10}
+   {:angle 60, :velocity 5.93, :wind 10}
+   {:angle 67.5, :velocity 5.989, :wind 10}
+   {:angle 75, :velocity 6.167, :wind 10}
+   {:angle 82.5, :velocity 6.155, :wind 10}
+   {:angle 90, :velocity 6.18, :wind 10}
+   {:angle 97.5, :velocity 6.07, :wind 10}
+   {:angle 105, :velocity 5.977, :wind 10}
+   {:angle 112.5, :velocity 5.767, :wind 10}
+   {:angle 120, :velocity 5.503, :wind 10}
+   {:angle 127.5, :velocity 5.087, :wind 10}
+   {:angle 135, :velocity 4.806, :wind 10}
+   {:angle 142.5, :velocity 4.659, :wind 10}
+   {:angle 150, :velocity 4.59, :wind 10}
+   {:angle 157.5, :velocity 4.456, :wind 10}
+   {:angle 165, :velocity 4.425, :wind 10}
+   {:angle 172.5, :velocity 4.395, :wind 10}
+   {:angle 180, :velocity 4.407, :wind 10}
+   {:angle 0, :velocity 0, :wind 15}
 
-   {:angle 0
-    :wind 15
-    :velocity 0}
-   {:angle 37.5
-    :wind 15
-    :velocity 4.489}
-   {:angle 45
-    :wind 15
-    :velocity 5.620}
-   {:angle 52.5
-    :wind 15
-    :velocity 6.091}
-   {:angle 60
-    :wind 15
-    :velocity 6.434}
-   {:velocity 6.637
-    :wind 15
-    :angle 67.5}
-   {:velocity 6.780
-    :wind 15
-    :angle 75}
-   {:velocity 6.735
-    :wind 15
-    :angle 82.5}
-   {:wind 15
-    :angle 90
-    :velocity 6.891}
-   {:wind 15
-    :angle 97.5
-    :velocity 6.741}
-   {:wind 15
-    :angle 105
-    :velocity 6.654}
-   {:wind 15
-    :angle 112.5
-    :velocity 6.525}
-   {:wind 15
-    :angle 120
-    :velocity 6.385}
-   {:wind 15
-    :angle 127.5
-    :velocity 6.259}
-   {:wind 15
-    :angle 135
-    :velocity 6.241}
-   {:wind 15
-    :angle 142.5
-    :velocity 6.091}
-   {:wind 15
-    :angle 150
-    :velocity 6.077}
-   {:wind 15
-    :angle 157.5
-    :velocity 6.002}
-   {:wind 15
-    :angle 165
-    :velocity 6.0000}
-   {:wind 15
-    :angle 172.5
-    :velocity 5.995}
-   {:wind 15
-    :angle 180
-    :velocity 5.977}
+   {:angle 37.5, :velocity 4.489, :wind 15}
+   {:angle 45, :velocity 5.62, :wind 15}
+   {:angle 52.5, :velocity 6.091, :wind 15}
+   {:angle 60, :velocity 6.434, :wind 15}
+   {:angle 67.5, :velocity 6.637, :wind 15}
+   {:angle 75, :velocity 6.78, :wind 15}
+   {:angle 82.5, :velocity 6.735, :wind 15}
+   {:angle 90, :velocity 6.891, :wind 15}
+   {:angle 97.5, :velocity 6.741, :wind 15}
+   {:angle 105, :velocity 6.654, :wind 15}
+   {:angle 112.5, :velocity 6.525, :wind 15}
+   {:angle 120, :velocity 6.385, :wind 15}
+   {:angle 127.5, :velocity 6.259, :wind 15}
+   {:angle 135, :velocity 6.241, :wind 15}
+   {:angle 142.5, :velocity 6.091, :wind 15}
+   {:angle 150, :velocity 6.077, :wind 15}
+   {:angle 157.5, :velocity 6.002, :wind 15}
+   {:angle 165, :velocity 6.0, :wind 15}
+   {:angle 172.5, :velocity 5.995, :wind 15}
+   {:angle 180, :velocity 5.977, :wind 15}
+   {:angle 0, :velocity 0, :wind 20}
 
-   {:angle 0
-    :wind 20
-    :velocity 0}
-   {:angle 37.5
-    :wind 20
-    :velocity 4.489}
-   {:angle 45
-    :wind 20
-    :velocity 5.675}
-   {:angle 52.5
-    :wind 20
-    :velocity 6.152}
-   {:angle 60
-    :wind 20
-    :velocity 6.434}
-   {:velocity 6.637
-    :wind 20
-    :angle 67.5}
-   {:velocity 6.992
-    :wind 20
-    :angle 75}
-   {:velocity 7.089
-    :wind 20
-    :angle 82.5}
-   {:wind 20
-    :angle 90
-    :velocity 7.235}
-   {:wind 20
-    :angle 97.5
-    :velocity 7.230}
-   {:wind 20
-    :angle 105
-    :velocity 7.086}
-   {:wind 20
-    :angle 112.5
-    :velocity 7.024}
-   {:wind 20
-    :angle 120
-    :velocity 6.942}
-   {:wind 20
-    :angle 127.5
-    :velocity 6.830}
-   {:wind 20
-    :angle 135
-    :velocity 6.834}
-   {:wind 20
-    :angle 142.5
-    :velocity 6.816}
-   {:wind 20
-    :angle 150
-    :velocity 6.841}
-   {:wind 20
-    :angle 157.5
-    :velocity 6.818}
-   {:wind 20
-    :angle 165
-    :velocity 6.787}
-   {:wind 20
-    :angle 172.5
-    :velocity 6.787}
-   {:wind 20
-    :angle 180
-    :velocity 6.840}])
-
+   {:angle 37.5, :velocity 4.489, :wind 20}
+   {:angle 45, :velocity 5.675, :wind 20}
+   {:angle 52.5, :velocity 6.152, :wind 20}
+   {:angle 60, :velocity 6.434, :wind 20}
+   {:angle 67.5, :velocity 6.637, :wind 20}
+   {:angle 75, :velocity 6.992, :wind 20}
+   {:angle 82.5, :velocity 7.089, :wind 20}
+   {:angle 90, :velocity 7.235, :wind 20}
+   {:angle 97.5, :velocity 7.23, :wind 20}
+   {:angle 105, :velocity 7.086, :wind 20}
+   {:angle 112.5, :velocity 7.024, :wind 20}
+   {:angle 120, :velocity 6.942, :wind 20}
+   {:angle 127.5, :velocity 6.83, :wind 20}
+   {:angle 135, :velocity 6.834, :wind 20}
+   {:angle 142.5, :velocity 6.816, :wind 20}
+   {:angle 150, :velocity 6.841, :wind 20}
+   {:angle 157.5, :velocity 6.818, :wind 20}
+   {:angle 165, :velocity 6.787, :wind 20}
+   {:angle 172.5, :velocity 6.787, :wind 20}
+   {:angle 180, :velocity 6.84, :wind 20}])
